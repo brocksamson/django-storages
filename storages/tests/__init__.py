4dcc3736e5c8ff6cb75e8233b7aa26dc4e7cb540
Matthew Miller <matthew.miller@alertmedia.com>
2013-11-07 12:53:42 -0600

    skip s3 tests if S3 boto not in packages

--- storages/tests/__init__.py
+++ storages/tests/__init__.py
@@ -1,2 +1,5 @@
-from storages.tests.hashpath import *
-from storages.tests.s3boto import *
+try:
+    from storages.tests.hashpath import *
+    from storages.tests.s3boto import *
+except ImportError:
+    pass
